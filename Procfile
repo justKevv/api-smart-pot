2721ac1055c68e934530efd01bf3866198869a78
justKevv <kevinbramastaa@gmail.com>
2025-05-01 12:46:54 +0700

    bismillahiso

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bash install_libgl.sh && python main.py
+web: python main.py
--- install_libgl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-# Install libGL.so.1 at runtime
-apt-get update && apt-get install -y libgl1 && apt-get clean
